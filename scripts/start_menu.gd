5fed6ddb91053d78adee8e0774adbad490a27177
Tim Kuessner <24401@nayland.school.nz>
2025-08-01 11:43:26 +1200

    start_menu

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
 func _on_start_button_pressed():
-    get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_options_button_pressed():
-    print("Options clicked")
+	print("Options clicked")
 
 func _on_quit_button_pressed():
-    get_tree().quit()
+	get_tree().quit()
